38d918b366735232dff0ccf57df14a6c2b9726b5
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-01 15:14:50 +0000

    Make appearance more accurate using css guided html elements

--- signup.php
+++ signup.php
@@ -3,21 +3,32 @@
 <div>
 <form action="signup-submit.php" method="post">
 	<div>
-		Name: <input type="text" name="name" maxlength="16" /> <br/>
-		Gender: <input type="radio" name="gen" value="male" /> Male
-						<input type="radio" name="gen" value="male" checked="checked" /> Female <br/>
-		Age: <input type="text" name="age" maxlength="6" /> <br/>
-		Personality Type: <input type="text" size="6" maxlength="4" />
-			<a href = "http://www.humanmetrics.com/cgi-win/JTypes2.asp">(Don't know your type?)</a> <br/>
-			<select name="favoriteos">
-				<option selected="selected">Windows</option>
-				<option>Mac OS X</option>
-				<option >Linux</option>
-			</select> <br/>
-		Seeking age:  <input type="text" name="min" size="6" maxlength="2" placeholder="min"/>
-									<input type="text" name="max" size="6" maxlength="2" placeholder="max"/>
-									<br/>
-		<input type="submit" />
+		<fieldset>
+			<legend>New User Signup</legend>
+			<ul>
+				<li>
+					<strong>Name:</strong> <input type="text" name="name" maxlength="16" />
+				</li>
+				<li><strong>Gender:</strong><input type="radio" name="gen" value="male" /> Male
+								<input type="radio" name="gen" value="male" checked="checked" /> Female</li>
+				<li><strong>Age:</strong><input type="text" name="age" maxlength="6" /> </li>
+				<li>
+					<strong>Personality Type:</strong><input type="text" size="6" maxlength="4" />
+					<a href = "http://www.humanmetrics.com/cgi-win/JTypes2.asp">(Don't know your type?)</a>
+				</li>
+				<li><strong>Favorite OS:</strong><select name="favoriteos">
+						<option selected="selected">Windows</option>
+						<option>Mac OS X</option>
+						<option >Linux</option>
+					</select>
+				</li>
+				<li>
+					<strong>Seeking age:</strong><input type="text" name="min" size="6" maxlength="2" placeholder="min"/>
+												<input type="text" name="max" size="6" maxlength="2" placeholder="max"/>
+				</li>
+			</ul>
+			<input type="submit" value ="Sign Up" />
+		</fieldset>
   </div>
 </form>
 </div>
