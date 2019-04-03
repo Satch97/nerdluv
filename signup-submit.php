e9b4f4f45f4d711af159552722765475393f2643
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-03 01:11:41 +0000

    Capture values from request

---
+++ signup-submit.php
@@ -0,0 +1,19 @@
+<?php include("top.html"); ?>
+
+
+<?php
+  $name = $_POST["name"];
+  $gen = $_POST["gen"];
+  $age =$_POST["age"];
+  $pers = $_POST["perstype"];
+  $os = $_POST["os"];
+  $min = $_POST["min"];
+  $max = $_POST["max"];
+
+  $err = FALSE;
+
+
+?>
+
+
+<?php include("bottom.html"); ?>
