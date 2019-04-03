b8a77a8452d7f6dd1bec8d7c46e1d54788909861
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-03 04:24:30 +0000

    Read all profiles from database

--- matches-submit.php
+++ matches-submit.php
@@ -14,5 +14,26 @@
 
 <?php
   }
+
+
+
+
+  try {
+    $db = new PDO('mysql:dbname=nerdluv;host=localhost', 'nerd', 'Nerdluv');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $rows = $db->query("SELECT i.name, i.gender, p.u_pers, i.age, oo.os_name, a.min, a.max
+                FROM userinfo i
+                JOIN userpersonality p ON i.u_id = p.u_id
+                JOIN useros o ON i.u_id = o.u_id
+                JOIN os oo ON oo.os_id = o.os_id
+                JOIN useragerange a ON i.u_id = a.u_id;");
+  } catch (PDOException $ex) {
+?>
+    <p>Sorry, a database error occurred. Please try again later.</p> <p>(Error details: <?= $ex->getMessage() ?>)</p>
+    <?php
+}
+
+
+
 ?>
 <?php include("bottom.html"); ?>
