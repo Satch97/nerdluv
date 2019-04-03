ca865e6950f81e4c8c22606bf9d625f2d2dccea2
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-03 01:13:51 +0000

    Validate name age and gender

--- signup-submit.php
+++ signup-submit.php
@@ -13,6 +13,32 @@
   $err = FALSE;
 
 
+  if (!preg_match("/\S/", $name)) { // non-empty
+    $err = TRUE;
+?>
+    <h2>Error, empty name field,
+      <a href = "http://www.humanmetrics.com/cgi-win/JTypes2.asp">Guide</a>
+    </h2>
+
+<?php
+  }
+
+  if (!preg_match("/^\d{1,2}$/", $age)) {
+    $err = TRUE;
+?>
+    <h2>Error, invalid age field</h2>
+<?php
+  }
+
+  if (!preg_match("/^F|M$/", $gen)) {
+    $err = TRUE;
+?>
+    <h2>Suspicious submission</h2>
+<?php
+  }
+
+
+
 ?>
 
 
